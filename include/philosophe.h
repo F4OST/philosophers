2f47fa54ab58bb444b89c564b00691792f321e89
Matthias Prost <matthias.prost@epitech.eu>
2017-03-13 16:03:30 +0100

    PUSH SCRIPT

--- include/philosophe.h
+++ include/philosophe.h
@@ -5,12 +5,18 @@
 ** Login   <matthias.prost@epitech.eu@epitech.eu>
 **
 ** Started on  Mon Mar 13 14:48:38 2017 Matthias Prost
-** Last update Mon Mar 13 14:49:49 2017 Matthias Prost
+** Last update Mon Mar 13 15:55:03 2017 Matthias Prost
 */
 
 #ifndef _PHILOSOPHE_H_
 #define _PHILOSOPHE_H_
 
 #include <stdio.h>
+
+typedef struct	s_philo
+{
+  			int			nb_philo;
+  			int			rice;
+}								t_philo;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <matthias.prost@epitech.eu@epitech.eu>
 **
 ** Started on  Mon Mar 13 14:43:28 2017 Matthias Prost
-** Last update Mon Mar 13 14:50:32 2017 Matthias Prost
+** Last update Mon Mar 13 16:03:07 2017 Matthias Prost
 */
 
 #include "extern.h"
@@ -13,6 +13,16 @@
 
 int	main(int ac, char **av)
 {
-  printf("%d\n", RCFStartup(ac, av));
+  int			i;
+  t_philo	value;
+
+  i = 0;
+  while (av[++i])
+    {
+      if (strcmp(av[i], "-p") == 0)
+	value.nb_philo = atoi(av[i++]);
+      else if (strcmp(av[i], "-e") == 0)
+	value.rice = atoi(av[i++]);
+    }
   return(0);
 }
